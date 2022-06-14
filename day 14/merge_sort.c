e21a81cc0acf42319ed773e1fc021487e5e18e6e
Anant <anant312004@gmail.com>
2022-06-14 23:28:04 +0530

    Added day 14

---
+++ day 14/merge_sort.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // if only one number
+        // quit
+    // else
+        // sort left half of number
+        // sort right half of numbers
+        // merge sorted halves
+    return 0;
+}
---
+++ day 14/readme.md
@@ -0,0 +1,6 @@
+# Day 14
+
+### Today I learnt about :- 
+
+- Merge sort
+- Solved lab3 of week 3 in cs50x course.
