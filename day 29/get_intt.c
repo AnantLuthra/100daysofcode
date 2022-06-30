27df3e4fab5a3ddf2ab201f9b709e9ece9d3ca4d
Anant <anant312004@gmail.com>
2022-06-30 23:28:59 +0530

    Added code code files for day 29

--- day 29/get_intt.c
+++ day 29/get_intt.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* get_int(char *string);
+int get_int(char *string);
 
 int main(void){
-    int *age, *height;
 
-    age = get_int("Enter your age: ");
-    height = get_int("Enter your height: ");
+    int age = get_int("Enter your age: ");
+    int height = get_int("Enter your height: ");
     printf("Your age is - %i, & height is - %i\n", age, height);
 }
 
-int* get_int(char *string) {
+int get_int(char *string) {
 
     int *a;
-
     a = malloc(sizeof(int));
-    
+
+    // printing out the console input statement.
     printf("%s", string);
     scanf("%i", a);
 
-    return a;
+    // Putting a pointer's value into b variable.
+    int b = *a;
+
+    // Free the space after the work is done.
+    free(a);
+
+    return b;
 }
