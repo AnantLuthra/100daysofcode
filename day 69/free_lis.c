1eefeb32ac3489dfa4e03adb0bd018a227bc21b1
Anant <anant312004@gmail.com>
2022-08-12 22:47:46 +0530

    Learned about how we free linked list in memory

---
+++ day 69/free_lis.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
+int main(void)
+{
+    // Creating main list variable which we will use longer till we are using this list.
+    node *list = NULL;
+
+    // Creating a n temp variable for adding new element to linked list.
+    node *n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+
+    // Adding integer value to the first node.
+    n->number = 1;
+    n->next = NULL;
+
+    // Copying address of first node to list variable.
+    // So that we can access that afterwords.
+    list = n;
+
+    // Adding second number to list.
+    n = malloc(sizeof(node));
+
+    // Checking if no free memory is available in computer.
+    if (n == NULL)
+    {
+        free(list);
+    }
+
+    n->number = 2;
+    n->next = NULL;
+    
+    // Adding the second element to linked list by putting the address
+    // second node to first node's next field.
+    list->next = n;
+
+    // Adding third node.
+    n = malloc(sizeof(node));
+    if (n == NULL) // If computer went out of memory.
+    {
+        // Keep in mind to free list in descending order,
+        // otherwise you're gonan touch the memory that you have free already.
+        free(list->next); // free the second node.
+        free(list); // Free the first node.
+        return 1;
+    }
+
+    n->number = 3;
+    n->next = NULL; // Setting third node's next field empty for adding
+                                            // next element to list.
+
+    list->next->next = n;   // adding third node's address into second node's next field.
+
+    // Checking if the list is null as if the list->next is null on any node
+    // it means that list have no more elements.
+    while (list != NULL)
+    {                   
+        node *temp = list->next;  // getting the address of next node.
+        free(list);    // free one node from which the address of next node is taken into temp.
+        list = temp;   // copying address of node next to the node which was just freed.
+    }
+}
---
+++ day 69/readme.md
@@ -0,0 +1,3 @@
+# Day 69
+
+- Today I learnt how do we free the memory of linked list.
